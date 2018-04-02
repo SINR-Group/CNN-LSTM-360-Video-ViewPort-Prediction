eb26a8e5e1f5f274b98d2049795bbf66c1fe2328
Mallesh Dasari <mdasari@cs.stonybrook.edu>
2018-04-02 17:45:32 -0400

    adding updated model with new resolution data

---
+++ prediction.py
@@ -0,0 +1,95 @@
+from __future__ import absolute_import
+from __future__ import print_function
+import os
+import glob
+import random
+import numpy as np
+from keras import optimizers
+from keras.layers import LSTM
+from keras.models import Sequential, Model
+from keras.applications.vgg16 import VGG16
+from keras.layers.wrappers import TimeDistributed
+from keras.applications.mobilenet import MobileNet
+from keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense, GlobalAveragePooling2D
+from keras.layers import Input
+from keras.layers.core import Activation, Flatten, Reshape
+from keras.layers.convolutional import Convolution2D, MaxPooling2D, UpSampling2D
+from keras.layers.normalization import BatchNormalization
+from keras.utils import np_utils
+from keras.applications import imagenet_utils
+
+
+
+# In[3]:
+
+
+height = 120
+width = 240
+path = '../datasets/'
+allfiles = glob.glob(path+'sensory/tile/*.npy')
+sali = glob.glob(path+'content/saliencyImages/*.npy')
+motion = glob.glob(path+'content/motionImages/*.npy')
+prob = glob.glob(path+'sensory/tileProb/*.npy')
+
+
+# In[4]:
+
+
+def myGenerator():
+    while True:
+        index_list = random.sample(range(1, 30000), 5)
+        alldata_x = []
+        alldata_y = []
+        for i in index_list:
+            f = allfiles[i]
+            s = f.split('_')
+            saliFile = '../datasets/content/saliencyImages/'+s[0][25:]+'_saliency_'+s[2].split('.')[0]+'.npy'
+            motionFile = '../datasets/content/motionImages/'+s[0][25:]+'_motion_'+s[2].split('.')[0]+'.npy'
+            probFile = '../datasets/sensory/tileProb/'+s[0][25:]+'_user'+s[1][4:]+'_'+s[2].split('.')[0]+'.npy'
+            a = np.load(f)
+            b = np.load(saliFile)
+            c = np.load(motionFile)
+            d = [a, b, c]
+            alldata_x.append(d)
+            alldata_y.append(np.load(probFile))
+        alldata_x = np.array(alldata_x)
+        alldata_x = np.rollaxis(alldata_x, 1, 5)  
+        #alldata_x = alldata_x.reshape((32, 30, height, width, 3))
+        #alldata_x = np.swapaxes(alldata_x, 1, 4)
+        alldata_y = np.array(alldata_y)
+        yield alldata_x, alldata_y
+# x = myGenerator()
+# xtrain, ytrain = next(x)
+# print('xtrain shape:',xtrain.shape)
+# print('ytrain shape:',ytrain.shape)
+
+
+# load the numpy arrays from saliency, motion maps and sensor data
+sali = glob.glob(path+'content/saliencyImages/*.npy')
+motion = glob.glob(path+'content/motionImages/*.npy')
+sensory = glob.glob(path+'sensory/tile/*.npy')
+
+
+# In[6]:
+
+input_shape=(30, height, width, 3)
+def mySegNet(input_shape):
+    base_model  = MobileNet(input_shape=(224,224,3), include_top=False)
+    x = base_model.output
+    x = GlobalAveragePooling2D()(x)
+    cnn_model = Model(inputs=base_model.input, outputs=x)
+    
+    model = Sequential();
+    model.add(TimeDistributed(cnn_model, input_shape=input_shape))
+    model.add(TimeDistributed(Flatten()))
+    
+    model.add(LSTM(200, return_sequences=True))
+    model.compile(optimizer='adam', loss='mean_squared_error')
+    #print(model.summary())
+    return model
+    
+input_shape=(30, height, width, 3)
+model = mySegNet(input_shape)
+model.fit_generator(generator=myGenerator(),
+                    use_multiprocessing=True,
+                   steps_per_epoch=5, epochs=5)
